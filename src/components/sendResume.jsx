f0e714e2a1392e880f6eb070979a04428be064c3
lwz7512 <lwz7512@gmail.com>
2020-03-30 23:00:37 -0300

    lost files

---
+++ src/components/sendResume.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const SendResume = () => (
+  <div className="join-message">
+      <h3>Contact</h3>
+      <p>
+        Please send your resume to 
+        <a href="mailto:info@marinethinking.com">
+          info@marinethinking.com
+        </a>
+      </p>
+      <p>
+          We thank all applicants for their interest, but only those selected for an interview will be contacted.
+      </p>
+  </div>
+)
+
+export default SendResume
